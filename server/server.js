cc9a5f7b696b040b3f2119d418d8ef604d941de2
vrindavalaboju <115057630+vrindavalaboju@users.noreply.github.com>
2025-11-12 11:12:53 -0500

    it was actually add and delete images

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // Database connection pool
 const pool = new Pool({
